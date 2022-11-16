c5738b3d2af4bb9c5fdab14286a4fdf87c8dddb2
Colton Wilson <colton_wilson93@yahoo.com>
2022-11-16 15:47:32 -0800

    Completed project, could use more work on css for style and questions. Improve for different screen size

---
+++ assets/JS/questions.js
@@ -0,0 +1,31 @@
+var questions = [
+  {
+    title: "An array’s length can be evaluated with the what property?",
+    multiChoice: ["1. .length", "2. .log", "3. the console", "4. .loop"],
+    answer: "1. .length"
+  },
+
+  {
+    title: "Within a loop, the 'break' keyword may be used to do what?",
+    multiChoice: ["1. break your competitors code", "2. exit the loop immediately", "3. repeat the loop", "4. indicate a stopping condition"],
+    answer: "2. exit the loop immediately"
+  },
+
+  {
+    title: "Properties in a JavaScript oject are often refferred to as what?",
+    multiChoice: ["1. dot walking", "2. key-value pairs", "3. nested properties", "4. undefined"],
+    answer: "2. key-value pairs"
+  },
+
+  {
+    title: "Which array method inserts an element at the end of the array?",
+    multiChoice: ["1. .pop()", "2. .push()", "3. .length", "4. .join()"],
+    answer: "2. .push()"
+  },
+
+  {
+    title: "What is a callback function?",
+    multiChoice: ["1. a function that accepts an array as an argument", "2. I function that performs an HTTP request", "3. a data type similar to a string or a boolean", "4. a function that is passed into another function as an argument"],
+    answer: "4. a function that is passed into another function as an argument"
+  }
+];
